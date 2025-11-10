41d900700ed33e504627e59bf2f66630d38a40a7
wallyliou <wally950831@gmail.com>
2025-11-10 16:25:13 +0800

    v4 拖曳

--- app/src/main/java/tw/edu/pu/csim/wallyliou/race/GameViewModel.kt
+++ app/src/main/java/tw/edu/pu/csim/wallyliou/race/GameViewModel.kt
@@ -4,13 +4,39 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.collections.plusAssign
 
 class GameViewModel: ViewModel() {
     var screenWidthPx by mutableStateOf(0f)
         private set
-
     var screenHeightPx by mutableStateOf(0f)
         private set
+    var gameRunning by mutableStateOf(false)
+    var circleX by mutableStateOf(0f)
+    var circleY by mutableStateOf(0f)
+
+
+    fun StartGame() {
+//回到初使位置
+        circleX = 100f
+        circleY = screenHeightPx - 100f
+        viewModelScope.launch {
+            while (gameRunning) { // 每0.1秒循環
+                delay(100)
+                circleX += 10
+                if (circleX >= screenWidthPx - 100){
+                    circleX = 100f
+                }
+            }
+        }
+    }
+    fun MoveCircle(x: Float, y: Float) {
+        circleX += x
+        circleY += y
+    }
 
     // 設定螢幕寬度與高度
     fun SetGameSize(w: Float, h: Float) {
